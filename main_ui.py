b23024c6ecb02e60e73d62797e2bb8d5d678221d
Aryan D <aryan.d.dalal@gmail.com>
2023-10-09 13:40:06 +0530

    streamlit

---
+++ main_ui.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import time
+
+user_input = st.text_input("Enter query")
+
+
+#def is_waranty (text : str) -> bool:
+#    return True
+#warrant_info : str
+#if is_waranty(user_input):
+#    warrant_info = st.text_input("enter warranty info")
+
+
+#store your response text here
+response : str = user_input
+
+#dont touch this guy
+pr_buff : str = ''
+resp_text = st.text('')
+for c in response:
+    pr_buff = pr_buff.join(['',str(c)])
+    resp_text.text(pr_buff)
+    time.sleep(0.1)
+
+
+anl = st.code('analysis : %s' % user_input)
