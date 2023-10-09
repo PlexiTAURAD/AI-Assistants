a03470599973b19f21d3d73e0f4261fd8d3a1b44
Aryan D <aryan.d.dalal@gmail.com>
2023-10-09 13:41:02 +0530

    added comment

--- main_ui.py
+++ main_ui.py
@@ -4,9 +4,10 @@ import time
 
 user_input = st.text_input("Enter query")
 
-
+#to handle warranty
 #def is_waranty (text : str) -> bool:
 #    return True
+
 #warrant_info : str
 #if is_waranty(user_input):
 #    warrant_info = st.text_input("enter warranty info")
